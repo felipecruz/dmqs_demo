8d81f85bc6873fdb435d3e41365530afdd7fdf60
Felipe cruz <felipecruz@loogica.net>
2012-08-10 02:07:21 -0300

    create SlowTest and FastTest to better show
    dmqs working

--- dmqs_app/tests/test_models.py
+++ dmqs_app/tests/test_models.py
@@ -1,8 +1,569 @@
-#from django.test import TestCase
+from django.test import TestCase
 from dmqs.integration.testcase import MemoryTestCase
 from dmqs_app.models import Friend, BestFriend, Dog, Friendship
 
-class TestFriend(MemoryTestCase):
+class FastTest(MemoryTestCase):
+    fixtures = ['dmqs_app/tests/fixtures/fixture.json']
+
+    def test_friend(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend2(self):
+        friend = Friend.objects.get(id=6)
+        friends = friend.friends.all()
+        friend2 = Friend.objects.get(id=7)
+        best_friends = friend2.best_friends.all()
+        best2 = BestFriend.objects.get(id=2)
+
+        self.assertTrue(friend != None)
+        self.assertTrue(len(friends) == 1)
+        self.assertTrue(len(best_friends) == 1)
+        self.assertTrue(list(friend2.best_friends.all()) == [best2])
+
+    def test_friend3(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend4(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend5(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend6(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend7(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend8(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend9(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend10(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend11(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend12(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend13(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend14(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend15(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend16(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend17(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend18(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend19(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend20(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend21(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend22(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend23(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend24(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend25(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend26(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend27(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend28(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend29(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend30(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend31(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend32(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend33(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend34(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend35(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend36(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend37(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend38(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend39(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend40(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend41(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend42(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend43(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend44(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend45(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend46(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend47(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend48(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend49(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend50(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend51(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend52(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend53(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend54(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend55(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend56(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend57(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend58(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend59(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend60(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend61(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend62(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend63(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend64(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend65(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend66(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend67(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend68(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend69(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend70(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend71(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend72(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend73(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend74(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend75(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend76(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend77(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend78(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend79(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend80(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend81(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend82(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend83(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend84(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend85(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend86(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend87(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend88(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend89(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend90(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend91(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend92(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend93(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend94(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend95(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend96(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend97(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend98(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend99(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend100(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend101(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend102(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend103(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend104(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend105(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend106(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend107(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend108(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend109(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+    def test_friend110(self):
+        friend = Friend.objects.get(id=1)
+
+        self.assertTrue(friend != None)
+
+
+class SlowTest(TestCase):
     fixtures = ['dmqs_app/tests/fixtures/fixture.json']
 
     def test_friend(self):
